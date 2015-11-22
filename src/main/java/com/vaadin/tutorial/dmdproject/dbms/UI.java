f4a56cdad2596b6a54acf1fbb131bf6d786e5d80
bbr88 <bisarnov@hotmail.com>
2015-11-22 12:27:18 +0300

    local

---
+++ src/main/java/com/vaadin/tutorial/dmdproject/dbms/UI.java
@@ -0,0 +1,17 @@
+package com.vaadin.tutorial.dmdproject.dbms;
+
+import java.io.IOException;
+
+public class UI {
+
+    public static void main(String[] args) throws IOException {
+
+        // checks system catalog and loads table names
+
+        UIHelper.init();
+        SystemCat.loadTable("test");
+
+
+
+    }
+}
